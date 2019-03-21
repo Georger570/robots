89ec1b2c963ac2daaf451588b42f6b71f9fe1295
Georger570 <39661699+Georger570@users.noreply.github.com>
2019-03-21 15:16:38 +0700

    Add files via upload

--- array.h
+++ array.h
@@ -9,7 +9,7 @@ typedef struct arr {
 	int **ptr;
 } arr;
 
-arr* InitArray(FILE *map_in)
+arr* InitArray_w_File(FILE *map_in)
 {
 		arr *array = malloc(sizeof(arr));
 		fscanf(map_in, "%d", &(array->n_rows));
@@ -33,19 +33,32 @@ arr* InitArray(FILE *map_in)
 		return array;
 }
 
+arr* InitArray(int n_rows,int n_columns)
+{
+		arr *array = malloc(sizeof(arr));
+		array->n_rows = n_rows;
+		array->n_columns = n_columns;
+		array->ptr = (int**) calloc(array->n_rows, sizeof(int*));			
+		for ( int i = 0; i<array->n_rows; i++)
+		{
+			array-> ptr[i] = (int*) calloc(array-> n_columns, sizeof(int));
+		}			
+		return array;
+}
+
 int PrintArray(arr* array)
 {
-	printf("\nNumber of rows: %d, \nNumber of columns: %d\n Array: \n", array->n_rows, array->n_columns);	
-	
+	printf("\nNumber of rows: %d,\nNumber of columns: %d,\nArray: \n", array->n_rows, array->n_columns);
 	for(int i = 0; i<(array->n_rows); i++)
 	{
 		for(int j = 0; j<(array->n_columns); j++)
 		{ 
-			printf("%d  ", array->ptr[i][j]);
+			printf("%d\t", array->ptr[i][j]);
 				
 		}
 		printf("\n");	
 	}
+	printf("\n");
 }
 
 #endif
--- main.c
+++ main.c
@@ -9,34 +9,18 @@ int main()
 	FILE *map_out;
 	if ((map_in = fopen("map.txt", "r")) != NULL)
 	{
-		arr *array = InitArray(map_in);		
+		arr *array = InitArray_w_File(map_in);		//Карта; 0 - свободно, 1 - препятсвие
 		fclose(map_in);
-		PrintArray(array);
-		//int iSize = array->n_rows;	//Размерность матрицы
-		//int iMap[array->n_rows][array->n_columns];	//Карта; 0 - свободно, 1 - препятсвие
-		printf("\n");			
-		int **iStepMap = (int**) calloc(array-> n_rows, sizeof(int*));
-		for ( int i = 0; i<array->n_columns; i++)
-		{
-			iStepMap[i] = (int*) calloc(array-> n_columns, sizeof(int));
-		}	//Карта шагов; содержит число шагов до ФИНИША в текущей координате;
-						// -1 - не посещали, -2 - препятствие
+		PrintArray(array);	
+		arr *iStepMap = InitArray(array->n_rows, array->n_columns);	//Карта шагов; содержит число шагов до ФИНИША в текущей координате;
+																					// -1 - не посещали, -2 - препятствие		
 		int *iResultX = (int*) calloc(array->n_rows * array->n_columns, sizeof(int));	//X-координата пути; [0] - точка НАЧАЛА
-		int *iResultY = (int*) calloc(array->n_rows * array->n_columns, sizeof(int));;	//Y-координата пути; [0] - точка НАЧАЛАa
+		int *iResultY = (int*) calloc(array->n_rows * array->n_columns, sizeof(int));	//Y-координата пути; [0] - точка НАЧАЛАa
 		coord start, end;
 		start.x = 1, start.y = 0, end.x = 9, end.y = 7;		
-		WaveStart(start, end,  array, iStepMap, iResultX, iResultY);
-		printf("\n");
-		for(int i = 0; i<(array->n_rows); i++)
-		{
-			for(int j = 0; j<(array->n_columns); j++)
-			{ 
-				printf("%d  ", iStepMap[i][j]);
-					
-			}
-			printf("\n");	
-		}
-		PathResult(start, end, array->n_rows, iStepMap, iResultX, iResultY);
+		WaveStart(start, end,  array, iStepMap->ptr, iResultX, iResultY);
+		PrintArray(iStepMap);
+		PathResult(start, end, array->n_rows, iStepMap->ptr, iResultX, iResultY);
 		map_out = fopen("map_out.txt", "w");
 		for (int i = 0; i<(array->n_rows*array->n_columns); i++)
 		{
@@ -53,3 +37,4 @@ int main()
 	
 	return 0;
 }
+
