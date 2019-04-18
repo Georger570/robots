46d8731d2d30b1924cb6599bceea883e433ebff6
AntEugene <Tosvering@gmail.com>
2019-04-18 20:22:48 +0700

    Modified to work without files.

--- MovementIntelligence.cpp
+++ MovementIntelligence.cpp
@@ -1,338 +1,200 @@
-public struct coord_direction
+#include "coord.h"
+#include "list.h"
+
+#define RightSensor 1
+#define LeftSensor 2
+#define FrontSensor 3
+#define RIGHT 1
+#define LEFT 2
+#define distance 21
+
+bool IsObstacleWithinReach(int a, int b)
 {
-	public int x;
-	public int z;
+    return true;
 }
 
-public struct coordinates
+void Turn(int a)
 {
-	public int x;
-	public int z;
 }
 
 class MovementIntelligence
 {
         //Robot
-        public Transform transform;
-        public RaycastHit hit;
-        public float speed = 1f;
-        public float speedValue = 10f;
-        public float directionValue = 1.0f;
-            
+        float speed = 1.0f;
+        float speedValue = 10.0f;
+
         //Position of Robot
-        coordinates CurrentPosition;
-        public coord_direction direction;
-        public Vector3 movement = new Vector3(0, 0, 0);
 
-        //"Covering all holes"
-        public bool BackFlag = true;
-        public int XX = 0;
-        public int ZZ = 0;
+        //coord* CurrentPosition = new coord(0,0); //Текущая позиция
+        //coord* direction = new coord(); //Текущее направление движения робота
+        coord CurrentPosition;
+        coord direction;
+        int movement = 0; //Переменная, отвечающая за расстояние, пройденное роботом
 
-        /*Files REPLACE_ME
-        WorkWithFiles Explored;
-        WorkWithFiles Unexplored;
-		*/
-		
-        public MovementIntelligence(WorkWithFiles File, WorkWithFiles File2)
+
+        //Files
+        //WorkWithFiles Explored;
+        //WorkWithFiles Unexplored;
+
+        //List
+        List* Explored;
+public:
+        MovementIntelligence(List* List1)
         {
-            Explored = File;
-            Unexplored = File2;
+            Explored = List1;
+            direction.setCoord(1,0);
+            CurrentPosition.setCoord(0,0);
         }
 
-        public void SetRobot(Transform r)
+        void setMovement(int mov)
         {
-            transform = r;
+            movement = mov;
         }
 
-        public void Init(Transform r)
+        /*void Moition() //Функция движения
         {
-            transform = r;
-            direction.x = 1;
-            direction.z = 0;
-            CurrentPosition.x = 0;
-            CurrentPosition.z = 0;
+            run_fb(dir, speed); //Передвижение робота
+            movement += transform.forward * (speed * directionValue) * Time.deltaTime; //Текущее растояние, пройденное роботом
+        }*/
+
+        coord  getFuturePosition(coord direction)
+        {
+            return (direction + CurrentPosition);
         }
 
-        public void Moition()
-        {
-            transform.position += transform.forward * (speed * directionValue) * Time.deltaTime;
-            movement += transform.forward * (speed * directionValue) * Time.deltaTime;
-        }
-
-        public coordinates  getFuturePosition()
-        {
-            coordinates FuturePosition;
-            FuturePosition.x = direction.x + CurrentPosition.x;
-            FuturePosition.z = direction.z + CurrentPosition.z;
-            return FuturePosition;
-        }
-
-        public coordinates getCurrentPosition()
+         coord getCurrentPosition() //Получить текущую позицию робота
         {
             return CurrentPosition;
         }
 
-        public bool AheadIsObstacle()
+         bool IsObstaclesAround() //Определение, есть ли препятсвие рядом
         {
-            if (Physics.Raycast(transform.position, transform.forward, out hit, (transform.localScale.x)))
+            if (IsObstacleWithinReach(FrontSensor, distance) || IsObstacleWithinReach(RightSensor, distance) || IsObstacleWithinReach(LeftSensor, distance))
                 return true;
             else return false;
         }
 
-        public bool RightIsObstacle()
+        bool TryToRegisterMotion(int robot_size) //Определение, прошёл ли робот расстояние, равное длине его корпуса
         {
-            if (Physics.Raycast(transform.position, transform.right, out hit, (transform.localScale.z)))
-                return true;
-            else return false;
-        }
-
-        public bool LeftIsObstacle()
-        {
-            if (Physics.Raycast(transform.position, -transform.right, out hit, (transform.localScale.z)))
-                return true;
-            else return false;
-        }
-
-        public bool IsObstaclesAround()
-        {
-            if (AheadIsObstacle() || RightIsObstacle() || LeftIsObstacle())
-                return true;
-            else return false;
-        }
-
-        public bool TryToRegisterMotion(float robot_size) //transform.localScale.z
-        {
-            if (Math.Abs(movement.x) >= robot_size)
+            if (movement >= robot_size)
             {
-                if (movement.x < 0)
-                    movement.x = movement.x + robot_size;
-                else if (movement.x > 0)
-                    movement.x = movement.x - robot_size;
-                CurrentPosition.x = CurrentPosition.x + direction.x;
-                CurrentPosition.z = CurrentPosition.z + direction.z;
+                movement -= robot_size;
+                CurrentPosition += direction;
                 //print(x + " " + z);
                 return true;
                 //WriteFlag = true;
 
-            }
-            else if (Math.Abs(movement.z) >= robot_size)
-            {
-                if (movement.z < 0)
-                    movement.z = movement.z + robot_size;
-                else if (movement.z > 0)
-                    movement.z = movement.z - robot_size;
-                CurrentPosition.x = CurrentPosition.x + direction.x;
-                CurrentPosition.z = CurrentPosition.z + direction.z;
-                //print(x + " " + z);
-                return true;
-                //WriteFlag = true;
             }
             return false;
         }
 
-        public bool CheckSide(Vector3 side)
+        bool CheckSide(int side)
         {
-            coord_direction side_direction;
-            if (side == transform.right)
+            coord side_direction(0,0);
+            if (side == RIGHT)
             {
                 side_direction = Direction_if_TurnRight(direction);
-                return Explored.isAlreadyinFile(getPosition(side_direction, CurrentPosition));
+                return Explored->isAlreadyInList(getFuturePosition(side_direction));
             }
-            else if (side == -transform.right)
+            else if (side == LEFT)
             {
                 side_direction = Direction_if_TurnLeft(direction);
-                return Explored.isAlreadyinFile(getPosition(side_direction, CurrentPosition));
-            }
-            else if (side == -transform.forward)
-            {
-                side_direction = Direction_if_TurnAround(direction);
-                return Explored.isAlreadyinFile(getPosition(side_direction, CurrentPosition));
+                return Explored->isAlreadyInList(getFuturePosition(side_direction));
             }
             return false;
         }
 
-        public coordinates getPosition(coord_direction direction, coordinates CurrentPosition)
+        bool Emergency()
         {
-            coordinates FuturePosition;
-            FuturePosition.x = CurrentPosition.x + direction.x;
-            FuturePosition.z = CurrentPosition.z + direction.z;
-            return FuturePosition;
-        }
-
-        public bool Emergency()
-        {
-
             return true;
         }
 
-        public int TurnFunction(int turnAngle, ref bool BackFlag)
+        int TurnFunction()
         {
-            /*if (hit.collider.tag != "Obstacle" || hit.collider == myColloder)
-            {
-                return 0;
-            }*/
-            coord_direction side_direction;
-            if (!(Physics.Raycast(transform.position, transform.right, out hit, (transform.localScale.x * 2)))
-                && (!CheckSide(transform.right) && BackFlag ))
-                                                        /*(((side_direction.x = Direction_if_TurnRight(direction).x + x) < (side_direction.x = Direction_if_TurnLeft(direction).x + x))
-                                                        || ((side_direction.z = Direction_if_TurnRight(direction).z + z) < (side_direction.z = Direction_if_TurnLeft(direction).z + z))))))*/
+            coord side_direction(0,0);
+            if (!IsObstacleWithinReach(RightSensor, distance)
+                && (!CheckSide(RIGHT)) )
             {
                 direction = Direction_if_TurnRight(direction);
-                transform.Rotate(0, turnAngle, 0);
+                Turn(RIGHT);
             }
-            else if (!(Physics.Raycast(transform.position, -transform.right, out hit, (transform.localScale.x * 2)))
-                && (!CheckSide(-transform.right) && BackFlag) )
+            else if (!IsObstacleWithinReach(LeftSensor, distance)
+                && (!CheckSide(LEFT)) )
             {
                 direction = Direction_if_TurnLeft(direction);
-                transform.Rotate(0, -turnAngle, 0);
-            }
-            else if (!(Physics.Raycast(transform.position, -transform.forward, out hit, (transform.localScale.z)))
-                && (!CheckSide(-transform.forward) && BackFlag))
-            {
-                direction = Direction_if_TurnAround( direction );
-                transform.Rotate(0, 2*turnAngle, 0);
-            }
-            else if (BackFlag == true)
-            {
-                //if (BackFlag == true)
-                BackFlag = false;
-                //AnotherFlag = 0;
-                return 0;
+                Turn(LEFT);
             }
             return 1;
         }
 
-        public bool IllBeBack(int turnAngle, ref bool BackFlag, ref int XX, ref int ZZ)
+        coord Direction_if_TurnRight(coord direction)
         {
-            if (speed == 0)
+            if (direction.x == 1)
             {
-                string[] SplitedStr = Unexplored.ReadLine().Split(';');
-                XX = int.Parse(SplitedStr[0]);
-                ZZ = int.Parse(SplitedStr[0]);
-                speed = 10;
+                direction.x = 0;
+                direction.y = 1;
             }
-                //var lines = System.IO.File.ReadAllLines("tif.txt");
-            //System.IO.File.WriteAllLines("tif.txt", lines.Skip(1).ToArray());
-            if (XX < CurrentPosition.x)
+            else if (direction.y == 1)
             {
-                while (direction.x != -1)
-                {
-                    if (!Physics.Raycast(transform.position, transform.right, out hit, ((transform.localScale.x))))
-                    {
-                        direction = Direction_if_TurnRight(direction);
-                        transform.Rotate(0, turnAngle, 0);
-                    }
-                    else break;
-                }
+                direction.y = 0;
+                direction.x = -1;
             }
-            else if (XX > CurrentPosition.x)
+            else if (direction.x == -1)
             {
-                while (direction.x != 1)
-                {
-                    if (!Physics.Raycast(transform.position, transform.right, out hit, ((transform.localScale.x))))
-                    {
-                        direction = Direction_if_TurnRight(direction);
-                        transform.Rotate(0, turnAngle, 0);
-                    }
-                    else break;
-                }
+                direction.x = 0;
+                direction.y = -1;
             }
-            else if (ZZ < CurrentPosition.z)
+            else if (direction.y == -1)
             {
-                while (direction.z != -1)
-                {
-                    if (!Physics.Raycast(transform.position, transform.right, out hit, ((transform.localScale.x))))
-                    {
-                        direction = Direction_if_TurnRight(direction);
-                        transform.Rotate(0, turnAngle, 0);
-                    }
-                    else break;
-                }
+                direction.y = 0;
+                direction.x = 1;
             }
-            else if (ZZ > CurrentPosition.z)
-            {
-                while (direction.z != 1)
-                {
-                    if (!Physics.Raycast(transform.position, transform.right, out hit, ((transform.localScale.x))))
-                    {
-                        direction = Direction_if_TurnRight(direction);
-                        transform.Rotate(0, turnAngle, 0);
-                    }
-                    else break;
-                }
-            }
-            if (CurrentPosition.x == XX && CurrentPosition.z == ZZ)
-                BackFlag = true;
-            return true;
+            return direction;
         }
+        coord Direction_if_TurnLeft(coord direction)
+        {
+            if (direction.x == 1)
+            {
+                direction.x = 0;
+                direction.y = -1;
+            }
+            else if (direction.y == -1)
+            {
+                direction.y = 0;
+                direction.x = -1;
+            }
+            else if (direction.x == -1)
+            {
+                direction.x = 0;
+                direction.y = 1;
+            }
+            else
+            {
+                direction.y = 0;
+                direction.x = 1;
+            }
+            return direction;
+        }
+        coord Direction_if_TurnAround(coord direction)
+        {
+            if (direction.x == 1)
+            {
+                direction.x = -1;
+            }
+            else if (direction.y == -1)
+            {
+                direction.y = 1;
+            }
+            else if (direction.x == -1)
+            {
+                direction.x = 1;
+            }
+            else
+            {
+                direction.y = -1;
+            }
+            return direction;
+        }
+    };
 
-        public coord_direction Direction_if_TurnRight(coord_direction direction)
-        {
-            if (direction.x == 1)
-            {
-                direction.x = 0;
-                direction.z = 1;
-            }
-            else if (direction.z == 1)
-            {
-                direction.z = 0;
-                direction.x = -1;
-            }
-            else if (direction.x == -1)
-            {
-                direction.x = 0;
-                direction.z = -1;
-            }
-            else if (direction.z == -1)
-            {
-                direction.z = 0;
-                direction.x = 1;
-            }
-            return direction;
-        }
-        public coord_direction Direction_if_TurnLeft(coord_direction direction)
-        {
-            if (direction.x == 1)
-            {
-                direction.x = 0;
-                direction.z = -1;
-            }
-            else if (direction.z == -1)
-            {
-                direction.z = 0;
-                direction.x = -1;
-            }
-            else if (direction.x == -1)
-            {
-                direction.x = 0;
-                direction.z = 1;
-            }
-            else
-            {
-                direction.z = 0;
-                direction.x = 1;
-            }
-            return direction;
-        }
-        public coord_direction Direction_if_TurnAround(coord_direction direction)
-        {
-            if (direction.x == 1)
-            {
-                direction.x = -1;
-            }
-            else if (direction.z == -1)
-            {
-                direction.z = 1;
-            }
-            else if (direction.x == -1)
-            {
-                direction.x = 1;
-            }
-            else
-            {
-                direction.z = -1;
-            }
-            return direction;
-        }
-    }
+
