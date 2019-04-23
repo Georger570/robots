450b5733a6afff6aa14bd992ac4c59383a6750c3
AntEugene <Tosvering@gmail.com>
2019-04-23 21:33:23 +0700

    Decommunization 1 ? help

--- WaveAlgorithm.h
+++ WaveAlgorithm.h
@@ -1,41 +1,35 @@
-//========================================================================
-//            ВОЛНОВОЙ АЛГОРИТМ нахождения кратчайшего пути
-//                   от точки A(x0;y0) до B(x1;y1)
-//
-//А.Даниловский ©26.02.2013
-//========================================================================
 #include "stdio.h"
 #include <stdbool.h>
 #include "array.h"
 #include "coord.h"
 /*
-const int iSize = 10;	//Размерность матрицы
-int iMap[10][10];	//Карта; 0 - свободно, 1 - препятсвие
-int iStepMap[10][10];	//Карта шагов; содержит число шагов до ФИНИША в текущей координате;
-						// -1 - не посещали, -2 - препятствие
-int iResultX[100];	//X-координата пути; [0] - точка НАЧАЛА
-int iResultY[100];	//Y-координата пути; [0] - точка НАЧАЛА
+const int iSize = 10;	//Matrix size;
+int iMap[10][10];	//Map; 0 - is free, 1 - wall;
+int iStepMap[10][10];	//Step map; countains nimber of steps to finish in the current coordinate;
+						// -1 - did't visit, -2 - wall
+int iResultX[100];	//X- way coordinate; [0] - Start point
+int iResultY[100];	//Y- way coordinate; [0] - Start point
 */
 //------------------------------------------------------------------------
 //------------------------------------------------------------------------
-//x0, y0 - начальная координата; x1, y1 - конечная координата
-//волну начинаем запускать с точки назначения
+//x0, y0 - Begin coordinate; x1, y1 - Finish coordinate
+//Start wave from Finish
 
 bool WaveStart(coord start, coord end, arr* iMap, int **iStepMap, int *iResultX, int *iResultY)
 {
-	//объявление и инициализация
+	//Initialization
 	int iStep = 0;
-	//Шаг; изначально равен 0
+	//Step;
 	//int iI;
-	bool bResult = false,	//Путь найден
-		bAdded = true;	//Внесены изменения; для контроля цикла
+	bool bResult = false,	//path found
+		bAdded = true;	//Made changes. To control the cycle;
 	for (int i = 0; i < (iMap -> n_rows); i++)
 		for (int j = 0; j < (iMap -> n_columns); j++)
 		{
 			if ((iMap -> ptr[i][j]) != 0)
-				iStepMap[i][j] = -2;	//препятствие
+				iStepMap[i][j] = -2;
 			else
-				iStepMap[i][j] = -1;	//не посещено
+				iStepMap[i][j] = -1;
 		}
 	for (int i = 0; i < (iMap -> n_rows * iMap -> n_columns); i++)
 	{
@@ -46,23 +40,23 @@ bool WaveStart(coord start, coord end, arr* iMap, int **iStepMap, int *iResultX,
 	//--------
 	iStepMap[end.x][end.y] = iStep;
 	//--------
-	//проверка координат на свободно/занято
+	//Free check
 	if ((iMap -> ptr[start.x][start.y]) == 1 || (iMap -> ptr[end.x][end.y]) == 1)
 	{
 		bResult = false;
 
 		return bResult;
 	}
-	//Начало ВОЛНЫ. пускаем волну
+	//Wave BEGIN.
 	while (bAdded && iStepMap[start.x][start.y] == -1)
 	{
-		bAdded = false;	//ничего еще не добавили
-		iStep++;	//Увеличиваем число шагов
+		bAdded = false;	
+		iStep++;
 
-		for (int i = 0; i < iMap -> n_rows; i++)	// Пробегаем по всей карте
+		for (int i = 0; i < iMap -> n_rows; i++)	// Check the whole map;
 			for (int j = 0; j < iMap -> n_columns; j++)
-				//Если точка с координатами (i;j) обрабатывалась на
-				//предыдущем шаге, то обрабатываем соседние клетки
+				//If point with coordinates (i;j) has been verifief
+				//in the previous step we do not check the adjacent cells;
 				if (iStepMap[i][j] == iStep - 1)
 				{
 					int _i, _j;
@@ -71,8 +65,7 @@ bool WaveStart(coord start, coord end, arr* iMap, int **iStepMap, int *iResultX,
 
 					for (int k = 0; k < iMap -> n_rows; k++)
 					{
-						//последовательно перебираем соседние точки
-						//и обрабатываем их
+						//Check all points around
 						switch (k)
 						{
 						case 0: _i = i + 1; _j = j; break;
@@ -80,8 +73,7 @@ bool WaveStart(coord start, coord end, arr* iMap, int **iStepMap, int *iResultX,
 						case 2: _i = i; _j = j + 1; break;
 						case 3: _i = i; _j = j - 1; break;
 						}
-						//проверяем клетки на принадлежность матрице
-						//и на то, обрабатывалась ли она (-1)
+						//we check the cells for belonging to the matrix and whether it was processed
 						if (_i >= 0 && _j >= 0 && _i < iMap -> n_rows && _j < iMap -> n_rows)
 							if (iStepMap[_i][_j] == -1)
 							{
@@ -91,10 +83,7 @@ bool WaveStart(coord start, coord end, arr* iMap, int **iStepMap, int *iResultX,
 					}
 				}
 	}
-	//Конец ВОЛНЫ.
-	//Обработка результатов ВОЛНЫ
-	//Если начальная точка (x0,y0) не обработана (=-1, =-2), то ПУТЬ не найден
-	// -1 - не посещали, -2 - препятствие
+	//Wave END.
 	if (iStepMap[start.x][start.y] == -1 || iStepMap[start.x][start.y] == -2)
 		bResult = false;
 	else if (iStepMap[start.x][start.y] >= 0)
@@ -111,8 +100,8 @@ void PathResult(coord start, coord end, int iSize, int **iStepMap, int *iResultX
 {
 	int iStep = iStepMap[start.x][start.y];
 	int iI = 0;
-	int x = start.x, y = start.y;	//координаты предыдущей точки маршрута
-	bool bAdded = true;	//Внесены изменения; для контроля цикла
+	int x = start.x, y = start.y;	//coordinates of the previous waypoint
+	bool bAdded = true;
 	iResultX[iI] = start.x;
 	iResultY[iI] = start.y;
 
@@ -122,16 +111,15 @@ void PathResult(coord start, coord end, int iSize, int **iStepMap, int *iResultX
 		iResultY[iI] = end.y;
 		return;
 	}
-	//Начало СОХРАНЕНИЯ ПУТИ. пускаем волну
+	//Begin SAVE PATH. Wave Start
 	while (bAdded && iStep >= 0)
 	{
-		bAdded = false;	//ничего еще не добавили
-		iI++;		//номер следующей точки найденного маршрута
-		iStep--;	//Увеличиваем число шагов
-		for (int i = 0; i < iSize; i++)	// Пробегаем по всей карте
+		bAdded = false;
+		iI++;		//the number of the next point of the found route
+		iStep--;	//MORE steps
+		for (int i = 0; i < iSize; i++)
 			for (int j = 0; j < iSize; j++)
-				//Если точка с координатами (i;j) обрабатывалась на
-				//предыдущем шаге, то обрабатываем соседние клетки
+				//If the point with coordinates (i; j) was processed at the previous step, then we process the neighboring cells
 				if (iStepMap[i][j] == iStep)
 				{
 					int _i, _j;
@@ -139,9 +127,7 @@ void PathResult(coord start, coord end, int iSize, int **iStepMap, int *iResultX
 					_j = j;
 					
 					for (int k = 0; k < 4; k++)
-					{
-						//последовательно перебираем соседние точки
-						//и обрабатываем их
+					{	
 						switch (k)
 						{
 						case 0: _i = i; _j = j - 1; break;
@@ -149,7 +135,7 @@ void PathResult(coord start, coord end, int iSize, int **iStepMap, int *iResultX
 						case 2: _i = i; _j = j + 1; break;
 						case 3: _i = i - 1; _j = j; break;
 						}
-						//проверяем клетки на принадлежность матрице
+						//Check the cells for belonging to the matrix
 						if (_i >= 0 && _j >= 0 && _i < iSize && _j < iSize)
 							if (_i == x && _j == y)
 							{
@@ -163,7 +149,7 @@ void PathResult(coord start, coord end, int iSize, int **iStepMap, int *iResultX
 					}
 				}
 	}
-	//Конец СОХРАНЕНИЯ ПУТИ.
+	//End of the PATH SAVE.
 	iResultX[iI + 1] = end.x;
 	iResultY[iI + 1] = end.y;
 
